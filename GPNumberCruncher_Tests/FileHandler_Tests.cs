984b0ee55afda74674737ff640ab13d2cd9f0fff
Dave Tapson <dave.tapson@gmail.com>
2021-10-04 22:51:03 +1300

    Tests created, because we all love TDD...

---
+++ GPNumberCruncher_Tests/FileHandler_Tests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPNumberCruncher_Tests
+{
+    [TestClass]
+    public class FileHandler_Tests
+    {
+        [TestMethod]
+        public void CheckFileExists_Test()
+        {
+            // assemble
+
+            // act
+
+            // assert
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void GetSavedNumber_Test()
+        {
+            // assemble
+
+            // act
+
+            // assert
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void SaveNumber_Test()
+        {
+            // assemble
+
+            // act
+
+            // assert
+            Assert.IsTrue(false);
+        }
+
+    }
+}
---
+++ GPNumberCruncher_Tests/NumberHandler_Tests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPNumberCruncher_Tests
+{
+    [TestClass]
+    public class NumberHandler_Tests
+    {
+        [TestMethod]
+        public void CrunchNumbers_Below_152_Test()
+        {
+            // assemble
+            int expected = 1;
+
+            // act
+
+            // assert
+            Assert.AreEqual(expected, 2);
+        }
+
+        [TestMethod]
+        public void CrunchNumbers_Above_152_Test()
+        {
+            // assemble
+            int expected = 1;
+
+            // act
+
+            // assert
+            Assert.AreEqual(expected, 2);
+        }
+    }
+}
