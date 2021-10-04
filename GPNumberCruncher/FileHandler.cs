a24a9eba9c0b39b032400a1f46f021ef6077933f
Dave Tapson <dave.tapson@gmail.com>
2021-10-04 23:33:44 +1300

    FileHandler and NumberHandler classes done plus tests.

---
+++ GPNumberCruncher/FileHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPNumberCruncher
+{
+    public static class FileHandler
+    {
+        public static int GetNumberFromUser()
+        {
+            Console.WriteLine("Enter number:");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            return userNumber;
+        }
+
+        public static void CheckFileExists(string file)
+        {
+            if (!File.Exists(file))
+                SaveNumber(file, 0);
+        }
+
+        public static int GetSavedNumber(string filePath)
+        {
+            try
+            {
+                StreamReader file = new(filePath);
+
+                var line = file.ReadLine();
+
+                file.Close();
+
+                return Convert.ToInt32(line);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"GetSavedNumber - Error encountered: {ex.Message}");
+                throw;
+            }
+        }
+
+        public static void SaveNumber(string filePath, int number)
+        {
+            string path = filePath;
+
+            using FileStream fileStream = File.OpenWrite(path);
+
+            Byte[] info = new UTF8Encoding(true).GetBytes(number.ToString());
+
+            fileStream.Write(info, 0, info.Length);
+        }
+    }
+}
--- GPNumberCruncher_Tests/FileHandler_Tests.cs
+++ GPNumberCruncher_Tests/FileHandler_Tests.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using GPNumberCruncher;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,38 +12,87 @@ namespace GPNumberCruncher_Tests
     [TestClass]
     public class FileHandler_Tests
     {
+        string testFileName = "TestFile.txt";
+
         [TestMethod]
-        public void CheckFileExists_Test()
+        public void CheckFileExists_ExistingFile_Test()
         {
             // assemble
+            int expected = 1;
+
+            CreateFile(expected);
 
             // act
+            FileHandler.CheckFileExists(testFileName);
+
+            int actual = GetSavedNumber();
 
             // assert
-            Assert.IsTrue(false);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckFileExists_NoFile_Test()
+        {
+            // assemble
+            int expected = 0; // this is what file is initialised with if no file exists
+            File.Delete(testFileName);
+
+            // act
+            FileHandler.CheckFileExists(testFileName);
+            int actual = GetSavedNumber();
+
+            // assert
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void GetSavedNumber_Test()
         {
             // assemble
+            int expected = 2;
+
+            CreateFile(expected);
 
             // act
+            int actual = FileHandler.GetSavedNumber(testFileName);
 
             // assert
-            Assert.IsTrue(false);
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void SaveNumber_Test()
         {
             // assemble
+            int expected = 3;
 
             // act
+            FileHandler.SaveNumber(testFileName, expected);
+
+            int actual = GetSavedNumber();
 
             // assert
-            Assert.IsTrue(false);
+            Assert.AreEqual(expected, actual);
         }
 
+        private int GetSavedNumber()
+        {
+            StreamReader file = new(testFileName);
+            var line = file.ReadLine();
+            file.Close();
+
+            return Convert.ToInt32(line);
+        }
+
+        private void CreateFile(int expected)
+        {
+            FileStream fileStream = File.OpenWrite(testFileName);
+
+            Byte[] info = new UTF8Encoding(true).GetBytes(expected.ToString());
+
+            fileStream.Write(info, 0, info.Length);
+            fileStream.Close();
+        }
     }
 }
